afeeb31ec458055d32cb4c58d30b37d7450f312b
Sairam Harikrishnan <sai_hari98@hotmail.com>
2024-06-08 11:59:56 -0500

    created frontend and backend files

---
+++ playfy/src/main/java/com/saih/playfy/PlayfyApplication.java
@@ -0,0 +1,13 @@
+package com.saih.playfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayfyApplication.class, args);
+	}
+
+}
---
+++ playfy/src/test/java/com/saih/playfy/PlayfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saih.playfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
